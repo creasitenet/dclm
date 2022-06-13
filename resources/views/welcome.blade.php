fe1a6a9f0d0357b74fc925f8aa32dd0dd6ffb573
Edouard Boissel <edouardboissel@gmail.com>
2022-06-13 22:28:01 +0200

    Improve react loading in laravel mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,9 @@
         </style>
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        
+        <script src="{{ asset('js/manifest.js') }}" defer></script>
+        <script src="{{ asset('js/vendor.js') }}" defer></script>
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="antialiased">
