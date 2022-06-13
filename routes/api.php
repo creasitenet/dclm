89f51333ad8649429b2f5c287cbb619fd909c354
Edouard Boissel <edouardboissel@gmail.com>
2022-06-13 19:58:18 +0200

    Add api route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'api','prefix' => 'lemonde'],function (){
+    Route::get('/',LeMondeController::class . '@getUne')->name('api.lemonde.une');
+});
