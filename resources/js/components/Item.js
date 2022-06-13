2f21d79fe405e0aa70273cf1d931a0081bc1fd20
Edouard Boissel <edouardboissel@gmail.com>
2022-06-13 22:19:46 +0200

    Display item data into Item component

--- resources/js/components/Item.js
+++ resources/js/components/Item.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link, useLocation } from 'react-router-dom';
+import './Item.scss';
 
 export default function Item() {
     const location = useLocation();
@@ -12,13 +13,15 @@ export default function Item() {
                 <br />
                 <br />
                 <div className="row">
-                    <div className="col">
+                    <div className="col"></div>
+                    <div className="col-8">
                         <div className="item">
                             <h1>{item.title}</h1>
                             <img src={item.media} className="img-responsive" alt={item.title} />
                             <p className="description"> {item.description} </p>
                         </div>
                     </div>
+                    <div className="col"></div>
                 </div>
             </div>
         </>
